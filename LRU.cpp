381ea89b6445f8b48fee418d4cac8d7bb328de19
Prerit <40600315+Prerit-Jain@users.noreply.github.com>
2025-04-14 12:47:00 +0000

    Add the main function

--- LRU.cpp
+++ LRU.cpp
@@ -32,7 +32,6 @@ bool deleteOldRecords(int ttl, time_t crntTs)
     crnt_hash_idx = (crntTs >>9) & RECORD_SIZE;
     /* Get the Hash Idx where the records before ttl time is present */
     hash_idx_delete= ((crntTs - ttl)>>9)&(RECORD_SIZE);
-··
     printf("hash_idx_delete =%d, crnt_hash_idx =%d \n", hash_idx_delete, crnt_hash_idx);
     while(hash_idx_delete != crnt_hash_idx)
     {
@@ -45,7 +44,7 @@ bool deleteOldRecords(int ttl, time_t crntTs)
        {
           if( (crntTs - it->ts) >= ttl)
           {
-             printf("Delete the record- userId=%d,ts=%d \n", it->userId, it->ts);·
+             printf("Delete the record- userId=%d,ts=%d \n", it->userId, it->ts);
              it->userId = -1;
              bucket.erase(it);
           }
@@ -59,4 +58,26 @@ bool deleteOldRecords(int ttl, time_t crntTs)
        if(hash_idx_delete == 0)
          hash_idx_delete = RECORD_SIZE ;
     }
+}
+Record recDB[65536];
+int main()
+{
+    int duration;
+    int i, userId=1, ttl;
+    time_t ts ;
+
+    printf("Enter the ttl\n");
+    scanf("%d", &ttl);
+
+    time_t crntTs = time(nullptr);·
+    /*Insert the record 100 times after every 200ms */
+    for(i=0; i< 100; i++)
+    {
+       recDB[userId-1].userId= userId;
+       crntTs = crntTs + 200;
+       recDB[userId-1].ts = crntTs ;
+       insertRecord(recDB[userId-1]);
+       userId++;
+    }
+    deleteOldRecords(ttl, crntTs);
 }
